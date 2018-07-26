7835640921ae3d45c2466cd0be269b63a2a08a37
Edwin Mwaniki <edmwaniki@gmail.com>
2018-07-26 12:06:06 +0300

    Header color

---
+++ application/views/pages/about.php
@@ -0,0 +1,3 @@
+<h2><?= $title ?></h2>
+
+<p>CiBlog version 1.0</p>
---
+++ application/views/templates/header.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>ciBlog</title>
+	<link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css">
+
+  <style type="text/css">
+    .navbar-default {
+    background-color: #F8F8F8;
+    border-color: #E7E7E7;
+}
+  </style>
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
+        </div>
+        <div id="navbar">
+          <ul class="nav navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>/about">About</a></li>
+            
+          </ul>
+          
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
